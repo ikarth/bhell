a4c996005a262118393fafffc7cd6975b52f2e70
Isaac Karth <isaac@isaackarth.com>
2021-07-20 12:34:49 -0700

    turn off debugging visuals

--- src/main.js
+++ src/main.js
@@ -369,7 +369,7 @@ const config = {
         default: "arcade",
         arcade: {
             gravity: 0,
-            debug: true
+            debug: false
         }
     },
     scene: [Stage],
